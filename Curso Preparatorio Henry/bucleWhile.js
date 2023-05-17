8045d31e8f8ca812c24a3d86beaf86b555c47e51
LukeEsteban <para.cursos.leg@gmail.com>
2023-05-17 11:09:21 -0300

    clase sobre bucles

---
+++ Curso Preparatorio Henry/bucleWhile.js
@@ -0,0 +1,10 @@
+// Se utiliza cuando no tenemos 
+// la certeza de cuantos pasos
+// vamos a necesitar hasta 
+// finalizar la ejecución. 
+
+var suma = 0;
+while (suma<3) {
+    suma =suma+1;
+    console.log(suma)
+}
---
+++ Curso Preparatorio Henry/bucles.js
@@ -0,0 +1,12 @@
+// Se utiliza con antelación cuando 
+// sabemos la cantidad máxima exacta de 
+// pasos que queremos ejecutar
+
+var suma = 0;
+
+for ( var i=0; i<5; i++){
+    suma = suma + 1;
+    console.log('variable de iteracion : ', i);
+};
+    console.log('variable suma : ', suma);
+
